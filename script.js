44314417cba43f9f8ddac6a83fb10778aba715e4
miimi <Maryanabdulahi730@gmail.com>
2026-08-14 22:10:57 +0300

    89- commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const button = document.getElementById("learnBtn");
 const message = document.getElementById("message");
-
-
 button.addEventListener("click", function () {
     message.textContent = "Thank you for visiting my website!";
 });
